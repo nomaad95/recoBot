2caa99fab323edbf09730a0b176fa8eb647e660e
Camille <camille.bamboute@hotmail.fr>
2020-10-23 13:06:42 +0200

    add classification methodswq!

---
+++ classification.py
@@ -0,0 +1,54 @@
+import tflite_runtime.interpreter as tflite
+import cv2
+import numpy as np
+
+def create_category_index(label_path='label/labelmap.txt'):
+    f = open(label_path)
+    category_index = {}
+    for i, val in enumerate(f):
+        if i != 0:
+            val = val[:-1]
+            if val != '???':
+                category_index.update({(i-1): {'id': (i-1), 'name': val}})
+    f.close()
+    return category_index
+
+category = create_category_index()
+
+def classification(img):
+
+    interpreter = tflite.Interpreter("coco.tflite")
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    img = cv2.imread(img)
+    img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img_rgb = cv2.resize(img_rgb, (300,300), cv2.INTER_AREA)
+    img_rgb = img_rgb.reshape([1, 300,300, 3])
+
+    interpreter.set_tensor(input_details[0]['index'], img_rgb)
+    interpreter.invoke()
+
+    det_boxes = interpreter.get_tensor(output_details[0]['index'])
+    det_classes = interpreter.get_tensor(output_details[1]['index'])[0]
+    det_scores = interpreter.get_tensor(output_details[2]['index'])[0]
+    output = []
+    output.append(det_classes)
+    output.append(det_boxes)
+    output.append(det_scores)
+    return output
+
+
+def tweet(result):
+    reco = []
+    for obj in result[0]:
+        reco.append(category.get(obj).get("name"))
+    obj = reco[0]
+    score = int(result[2][0]*100)
+    tweet = str(obj) +" "+ str(score)+"%"
+    return tweet
+
+conv = classification('img.jpg')
+a = tweet(conv)
+print(a)
